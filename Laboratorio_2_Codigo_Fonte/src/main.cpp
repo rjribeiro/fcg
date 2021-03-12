e4ec610aef95c67f786f495e5cdfa4ab124f10d2
Rafael Ribeiro <rjribeiro@inf.ufrgs.br>
2021-03-12 20:03:55 -0300

    adicionado free camera

--- Laboratorio_2_Codigo_Fonte/src/main.cpp
+++ Laboratorio_2_Codigo_Fonte/src/main.cpp
@@ -280,13 +280,10 @@ int main()
         float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
         float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
 
-
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-        //glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-        //glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-        glm::vec4 camera_view_vector = glm::vec4(-x,-y,-z,0.0f);
+        glm::vec4 camera_position_c  = glm::vec4(0.0f, 0.5f, 2.5f, 0.0f); 
+        glm::vec4 camera_view_vector    = glm::vec4(x,y,-z,0.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
         glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
 
