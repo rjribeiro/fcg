73c4a8cae110f3e86c564c1f7d5e1847d64124cc
Rafael Ribeiro <rjribeiro@inf.ufrgs.br>
2021-03-19 16:53:07 -0300

    finalizado

--- Laboratorio_2_Codigo_Fonte/src/main.cpp
+++ Laboratorio_2_Codigo_Fonte/src/main.cpp
@@ -154,7 +154,7 @@ int main()
     // Criamos uma janela do sistema operacional, com 800 colunas e 800 linhas
     // de pixels, e com título "INF01047 ...".
     GLFWwindow* window;
-    window = glfwCreateWindow(800, 800, "INF01047 - Seu Cartao - Seu Nome", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "INF01047 - 00265830 - RAFAEL JUNIOR RIBEIRO", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
